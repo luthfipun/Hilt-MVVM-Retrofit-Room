5a35405754b87e822646c890fac7b78f9b4d5748
Luthfi Abdul Azis <luthfieabdulazis#gmail.com>
2021-04-24 17:26:34 +0700

    initial commit

---
+++ app/src/main/java/github/luthfipun/hiltmvi/MainActivity.kt
@@ -0,0 +1,11 @@
+package github.luthfipun.hiltmvi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
